8abd011e3959a48251c7003673d74cf6356af3c1
Jim Kang <jimkang@gmail.com>
2019-06-13 23:36:05 -0400

    Fix scopes; add note about Premium account.

--- tasks/wrap-redirect.js
+++ tasks/wrap-redirect.js
@@ -2,7 +2,7 @@ var redirectToAuth = require('./redirect-to-auth');
 var config = require('../config');
 
 const scopeString =
-  'user-read-playback-state user-modify-playback-state streaming app-remote-control';
+  'user-read-playback-state user-modify-playback-state streaming app-remote-control user-read-birthdate user-read-email user-read-private';
 
 function WrapRedirect(routeDict) {
   return wrapRedirect;
